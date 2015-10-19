824a000933c79769cf1b03cd44af632a0e3cfe88
André Lucas <andre.miranda@locaweb.com.br>
2015-10-19 21:35:58 -0200

    surgeons

--- config/application.rb
+++ config/application.rb
@@ -33,3 +33,5 @@ module Clinicaiama
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
+
+SECRETS = Rails.application.secrets
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts ''
+
+puts 'Creating Surgeon for test...'
+Surgeon.create! :name => "André Lucas", :cpf => "2131231", :phone => "1212121", :email => "sfsdf@teste", :address => "Portugues", :address_number => "1", :address_complement => "1", :district => "Simus", :city => "Sorocaba", :postal_code => "23232", :state => "SP"
+Surgeon.create! :name => "Pakita", :cpf => "4565465", :phone => "345354354", :email => "sfsdf@teste", :address => "Portugues", :address_number => "2", :address_complement => "1", :district => "Tiete", :city => "Ita", :postal_code => "323232", :state => "SP"
+Surgeon.create! :name => "Dé", :cpf => "23432434", :phone => "567786787", :email => "sfsdf@teste", :address => "Ingles", :address_number => "3", :address_complement => "1", :district => "Flora", :city => "Itapopoca", :postal_code => "323232", :state => "SP"
