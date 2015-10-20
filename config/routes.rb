c369d04c77e586aa76ddcd71df48b474552d6f0a
André Lucas <andre.miranda@locaweb.com.br>
2015-10-20 13:10:14 -0200

    entries

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,14 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  get '/address/search' => 'address#search_postal_code'
   root :to => 'home#index'
   resources :surgeons
   resources :users
+  resources :hospitals
+  resources :health_insurances
+  resources :patients
+  resources :anesthetists
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
