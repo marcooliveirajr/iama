9acb15da048a6712a013415a0a5b3c96481a6548
andre <drelucas@gmail.com>
2015-11-18 21:11:19 -0200

    seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,83 +1,38 @@
 Rails.application.routes.draw do
 
-  resources :maps
-  resources :health_terminologies
-  resources :size_surgeries
   devise_for :users
-  resources :bedrooms
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
   get '/address/search' => 'address#search_postal_code'
   root :to => 'home#index'
-  resources :surgeons do
-    collection do
-      get :search
-    end
-  end
+
   resources :users, path: 'customusers', shallow: true do
     get 'search', to: 'users#search', as: 'search', on: :collection
   end
   resources :anesthetists, shallow: true do
     get 'search', to: 'anesthetists#search', as: 'search', on: :collection
   end
-
-  resources :hospitals do
-    collection do
-      get :search
-    end
+  resources :surgeons, shallow: true do
+    get 'search', to: 'surgeons#search', as: 'search', on: :collection
   end
-  resources :health_insurances
-  resources :patients
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  resources :patients, shallow: true do
+    get 'search', to: 'patients#search', as: 'search', on: :collection
+  end
+  resources :hospitals, shallow: true do
+    get 'search', to: 'hospitals#search', as: 'search', on: :collection
+  end
+  resources :health_insurances, shallow: true do
+    get 'search', to: 'health_insurances#search', as: 'search', on: :collection
+  end
+  resources :health_terminologies, shallow: true do
+    get 'search', to: 'health_terminologies#search', as: 'search', on: :collection
+  end
+  resources :maps, shallow: true do
+    get 'search', to: 'maps#search', as: 'search', on: :collection
+  end
+  resources :size_surgeries, shallow: true do
+    get 'search', to: 'size_surgeries#search', as: 'search', on: :collection
+  end
+  resources :bedrooms, shallow: true do
+    get 'search', to: 'bedrooms#search', as: 'search', on: :collection
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,42 +9,77 @@ puts 'Creating Role...'
 ['registered', 'moderator', 'admin'].each do |role|
   Role.find_or_create_by({name: role})
 end
-puts 'Creating User Admin...'
-User.create! :name => "admin", :email => "admin@admin.com", :password => "inicial1234", :role_id => 3
+#puts 'Creating User Admin...'
+#User.create! :name => "admin", :email => "admin@admin.com", :password => "inicial1234", :role_id => 3
 
 puts 'Tests'
 puts 'Creating Surgeon for test...'
-Surgeon.create! :name => "André Lucas", :cpf => "71155072871", :phone => "159990987", :email => "teste@teste.com", :address => "Rua Portugues", :address_number => "1", :address_complement => "1", :district => "Simus", :city => "Sorocaba", :postal_code => "18440000", :state => "SP"
-Surgeon.create! :name => "Pakita", :cpf => "67770445230", :phone => "15987654", :email => "www@teste", :address => "Alamenda Portugues", :address_number => "2", :address_complement => "1", :district => "Tiete", :city => "Ita", :postal_code => "18440000", :state => "SP"
-Surgeon.create! :name => "Dé", :cpf => "40062472143", :phone => "1543678", :email => "qqqqq@teste", :address => "Avenida Ingles", :address_number => "3", :address_complement => "1", :district => "Flora", :city => "Itapopoca", :postal_code => "18440000", :state => "SP"
+Surgeon.find_or_create_by({:name => "André Lucas", :cpf => "71155072871", :phone => "159990987", :email => "teste@teste.com", :address => "Rua Portugues", :address_number => "1", :address_complement => "1", :district => "Simus", :city => "Sorocaba", :postal_code => "18440000", :state => "SP"})
+Surgeon.find_or_create_by({:name => "Pakita", :cpf => "67770445230", :phone => "15987654", :email => "www@teste", :address => "Alamenda Portugues", :address_number => "2", :address_complement => "1", :district => "Tiete", :city => "Ita", :postal_code => "18440000", :state => "SP"})
+Surgeon.find_or_create_by({:name => "Dé", :cpf => "40062472143", :phone => "1543678", :email => "qqqqq@teste", :address => "Avenida Ingles", :address_number => "3", :address_complement => "1", :district => "Flora", :city => "Itapopoca", :postal_code => "18440000", :state => "SP"})
 
 puts 'Creating Anesthetists for test...'
-Anesthetist.create! :name => "Markito", :cpf => "71155072871", :phone => "159987987", :email => "teste@teste.com", :address => "Rua de Teste", :address_number => "1", :address_complement => "1", :district => "Simus", :city => "Sorocaba", :postal_code => "18440000", :state => "SP"
-Anesthetist.create! :name => "José", :cpf => "67770445230", :phone => "1598854", :email => "www@teste", :address => "Alamenda das Angelicas", :address_number => "2", :address_complement => "1", :district => "Tiete", :city => "Ita", :postal_code => "18440000", :state => "SP"
-Anesthetist.create! :name => "Vicentino", :cpf => "40062472143", :phone => "15434678", :email => "qqqqq@teste", :address => "Avenida General", :address_number => "3", :address_complement => "1", :district => "Flora", :city => "Itapopoca", :postal_code => "18440000", :state => "SP"
+Anesthetist.find_or_create_by({:name => "Markito", :cpf => "71155072871", :phone => "159987987", :email => "teste@teste.com", :address => "Rua de Teste", :address_number => "1", :address_complement => "1", :district => "Simus", :city => "Sorocaba", :postal_code => "18440000", :state => "SP"})
+Anesthetist.find_or_create_by({:name => "José", :cpf => "67770445230", :phone => "1598854", :email => "www@teste", :address => "Alamenda das Angelicas", :address_number => "2", :address_complement => "1", :district => "Tiete", :city => "Ita", :postal_code => "18440000", :state => "SP"})
+Anesthetist.find_or_create_by({:name => "Vicentino", :cpf => "40062472143", :phone => "15434678", :email => "qqqqq@teste", :address => "Avenida General", :address_number => "3", :address_complement => "1", :district => "Flora", :city => "Itapopoca", :postal_code => "18440000", :state => "SP"})
 
 puts 'Creating Patients for test...'
-Patient.create! :name => "Paciente 1", :cpf => "71155072871", :phone => "15998987", :email => "teste@teste.com", :address => "Rua 7 de setembro", :address_number => "1", :address_complement => "1", :district => "Simus", :city => "Sorocaba", :postal_code => "18440000", :state => "SP"
-Patient.create! :name => "Paciente 2", :cpf => "67770445230", :phone => "1598854", :email => "www@teste", :address => "Alamenda das Angelicas", :address_number => "2", :address_complement => "1", :district => "Tiete", :city => "Ita", :postal_code => "18440000", :state => "SP"
-Patient.create! :name => "Paciente 3", :cpf => "40062472143", :phone => "15432678", :email => "qqqqq@teste", :address => "Avenida Padrão", :address_number => "3", :address_complement => "1", :district => "Flora", :city => "Itapopoca", :postal_code => "18440000", :state => "SP"
+Patient.find_or_create_by({:name => "Paciente 1", :cpf => "71155072871", :phone => "15998987", :email => "teste@teste.com", :address => "Rua 7 de setembro", :address_number => "1", :address_complement => "1", :district => "Simus", :city => "Sorocaba", :postal_code => "18440000", :state => "SP"})
+Patient.find_or_create_by({:name => "Paciente 2", :cpf => "67770445230", :phone => "1598854", :email => "www@teste", :address => "Alamenda das Angelicas", :address_number => "2", :address_complement => "1", :district => "Tiete", :city => "Ita", :postal_code => "18440000", :state => "SP"})
+Patient.find_or_create_by({:name => "Paciente 3", :cpf => "40062472143", :phone => "15432678", :email => "qqqqq@teste", :address => "Avenida Padrão", :address_number => "3", :address_complement => "1", :district => "Flora", :city => "Itapopoca", :postal_code => "18440000", :state => "SP"})
 
 puts 'Creating Hospitals for test...'
-Hospital.create! :name => "Hospital Santa Casa", :kind => "tipo 1", :status => true
-Hospital.create! :name => "Hospital Unimed", :kind => "tipo 1", :status => false
-Hospital.create! :name => "Hospital Modelo", :kind => "tipo 2", :status => true
+Hospital.find_or_create_by({:name => "Hospital Santa Casa", :kind => "tipo 1", :status => true})
+Hospital.find_or_create_by({:name => "Hospital Unimed", :kind => "tipo 1", :status => false})
+Hospital.find_or_create_by({:name => "Hospital Modelo", :kind => "tipo 2", :status => true})
 
 puts 'Creating Bedroons for test...'
-Bedroom.create! :name => "Quarto 1", :hospital_id => 1
-Bedroom.create! :name => "Quarto 2", :hospital_id => 1
-Bedroom.create! :name => "Quarto 3", :hospital_id => 1
-Bedroom.create! :name => "Quarto 1", :hospital_id => 2
-Bedroom.create! :name => "Quarto 2", :hospital_id => 2
-Bedroom.create! :name => "Quarto 3", :hospital_id => 2
-Bedroom.create! :name => "Quarto 1", :hospital_id => 3
-Bedroom.create! :name => "Quarto 2", :hospital_id => 3
-Bedroom.create! :name => "Quarto 3", :hospital_id => 3
+Bedroom.find_or_create_by({:name => "Quarto 1", :hospital_id => 1})
+Bedroom.find_or_create_by({:name => "Quarto 2", :hospital_id => 1})
+Bedroom.find_or_create_by({:name => "Quarto 3", :hospital_id => 1})
+Bedroom.find_or_create_by({:name => "Quarto 1", :hospital_id => 2})
+Bedroom.find_or_create_by({:name => "Quarto 2", :hospital_id => 2})
+Bedroom.find_or_create_by({:name => "Quarto 3", :hospital_id => 2})
+Bedroom.find_or_create_by({:name => "Quarto 1", :hospital_id => 3})
+Bedroom.find_or_create_by({:name => "Quarto 2", :hospital_id => 3})
+Bedroom.find_or_create_by({:name => "Quarto 3", :hospital_id => 3})
 
 puts 'Creating Health insureces for test...'
-HealthInsurance.create! :name => "Unimed", :kind => "tipo 1", :status => true
-HealthInsurance.create! :name => "Teste", :kind => "tipo 1", :status => false
-HealthInsurance.create! :name => "Maritima", :kind => "tipo 2", :status => true
+HealthInsurance.find_or_create_by({:name => "Unimed", :kind => "tipo 1", :status => true})
+HealthInsurance.find_or_create_by({:name => "Teste", :kind => "tipo 1", :status => false})
+HealthInsurance.find_or_create_by({:name => "Maritima", :kind => "tipo 2", :status => true})
+
+puts 'Creating Size Surgery for test...'
+SizeSurgery.find_or_create_by({:health_insurance_id => 1, :size => 1, :value => 10})
+SizeSurgery.find_or_create_by({:health_insurance_id => 1, :size => 2, :value => 20})
+SizeSurgery.find_or_create_by({:health_insurance_id => 1, :size => 3, :value => 30})
+SizeSurgery.find_or_create_by({:health_insurance_id => 2, :size => 1, :value => 10})
+SizeSurgery.find_or_create_by({:health_insurance_id => 2, :size => 2, :value => 20})
+SizeSurgery.find_or_create_by({:health_insurance_id => 2, :size => 3, :value => 30})
+SizeSurgery.find_or_create_by({:health_insurance_id => 3, :size => 1, :value => 10})
+SizeSurgery.find_or_create_by({:health_insurance_id => 3, :size => 2, :value => 20})
+SizeSurgery.find_or_create_by({:health_insurance_id => 3, :size => 3, :value => 30})
+
+puts 'Creating Health Terminologies for test...'
+HealthTerminology.find_or_create_by({:code_tuss => 1, 
+									 :description_tuss => "Procedimento 1", 
+									 :category_tuss => "Categoria 1",
+									 :ch_surgeon => 10,
+									 :size => 1,
+									 :number_aux => 11,
+									 :version => 100})
+HealthTerminology.find_or_create_by({:code_tuss => 2, 
+									 :description_tuss => "Procedimento 2", 
+									 :category_tuss => "Categoria 2",
+									 :ch_surgeon => 10,
+									 :size => 1,
+									 :number_aux => 11,
+									 :version => 100})
+HealthTerminology.find_or_create_by({:code_tuss => 3, 
+									 :description_tuss => "Procedimento 3", 
+									 :category_tuss => "Categoria 2",
+									 :ch_surgeon => 10,
+									 :size => 1,
+									 :number_aux => 11,
+									 :version => 100})
+
