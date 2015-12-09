a41eb151364581330a4ada8b2643f0b0c685ad9e
andre <drelucas@gmail.com>
2015-12-09 12:24:18 -0200

    test

--- app/services/map/finder_service.rb
+++ app/services/map/finder_service.rb
@@ -3,7 +3,21 @@ class Map
     class << self
       def find(params)
         params = params.with_indifferent_access.symbolize_keys
-        Map.where("#{params[:type]} like ?", "%#{params[:text]}%").all
+        #binding.pry
+        #Map.where("#{params[:type]} like ?", "%#{params[:text]}%").all
+
+				$param_type = params[:type]
+				case $param_type
+				when 'anesthetist'
+					Map.joins('LEFT OUTER JOIN "anesthetists" ON "maps"."anesthetist_id" = "anesthetists"."id"').where(anesthetists: { name: "%#{params[:text]}%" })
+			    #Map.joins(:anesthetists).where(anesthetists: { name: "%#{params[:text]}%" })
+				when 'patient'
+			    puts "little child"
+				when 'surgeon'
+			    puts "child"
+				when 'hospital'
+			    puts "youth"
+				end        
       end
     end
   end
