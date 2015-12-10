73b4722c57b3bb7ea2f7cd01800163501609123c
André Lucas <andre.miranda@locaweb.com.br>
2015-12-10 12:59:37 -0200

    map search

--- app/services/map/finder_service.rb
+++ app/services/map/finder_service.rb
@@ -5,17 +5,22 @@ class Map
         params = params.with_indifferent_access.symbolize_keys
         # binding.pry
         #Map.where("#{params[:type]} like ?", "%#{params[:text]}%").all
-				$param_type = params[:type]
-				case $param_type
-				when 'anesthetist'
-			    Map.joins(:anesthetist).where(anesthetists: { name: params[:text] })
-				when 'patient'
-			    Map.joins(:patient).where(patients: { name: params[:text] })
-				when 'surgeon'
-			    Map.joins(:surgeon).where(surgeons: { name: params[:text] })
-				when 'hospital'
-			    Map.joins(:hospital).where(hospitals: { name: params[:text] })
-				end
+        if params[:text] != ""
+  				$param_type = params[:type]
+  				case $param_type
+  				when 'anesthetist'
+  			    #Map.joins(:anesthetist).where(anesthetists: { name: params[:text] })
+            Map.joins(:anesthetist).where("name like ?", "%#{params[:text]}%")
+  				when 'patient'
+  			    Map.joins(:patient).where("name like ?", "%#{params[:text]}%")
+  				when 'surgeon'
+  			    Map.joins(:surgeon).where("name like ?", "%#{params[:text]}%")
+  				when 'hospital'
+  			    Map.joins(:hospital).where("name like ?", "%#{params[:text]}%")
+  				end
+        else
+          Map.all
+        end
       end
     end
   end
