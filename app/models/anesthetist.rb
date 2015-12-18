56715d5f0ace41fa4e6d109c70b70184b5e811cb
André Lucas <andre.miranda@locaweb.com.br>
2015-12-18 10:11:25 -0200

    messages

--- app/models/anesthetist.rb
+++ app/models/anesthetist.rb
@@ -1,8 +1,7 @@
 class Anesthetist < ActiveRecord::Base
   validates :name, presence: { message: :required_name }
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: :valid_email }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: :valid_email, allow_blank: true }
   # validates :seat_amount,
   #           numericality: { greater_than_or_equal_to: 4, less_than_or_equal_to: 70, message: 'Quantidade deve ser maior que 3 e menor que 71' },
   #           presence:     { message: 'Necessário informar a quantidade de assentos.' }
-
 end
