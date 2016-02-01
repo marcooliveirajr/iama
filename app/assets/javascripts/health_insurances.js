04d1ab1ceb0b053039c64a91f4a6fcbf270254da
david <david.tstp@gmail.com>
2016-02-01 00:47:50 -0200

    create js

--- app/assets/javascripts/health_insurances.js
+++ app/assets/javascripts/health_insurances.js
@@ -1,17 +1,13 @@
-$( document  ).ready(function() {
-    $('#map_health_terminology_id').html("");
+$( document ).ready(function() {
+  $('#map_health_insurance_id').change(function(){
+    var id_hospital = $(this).val();
 
-    $('#map_health_insurance_id').change(function(){
-      var insurance_id = $('#map_health_insurance_id option:selected').val();
-
-        $('#map_health_terminology_id').html("");
-        $.getJSON("/health_insurance/health_terminology/"+ insurance_id +".json", function(result){
-           var terminology_options = $('#map_health_terminology_id');
-           $.each(result, function(item, element) {
-             console.log(element.to_s);
-             terminology_options.append($("<option />").val(element.id).text(element.to_s));
-             console.log(terminology_options.html());
-           });
-         });
-     });
+    $.getJSON("/terminology/hospital.json?hospital_id="+ id_hospital, function(result){
+      var terminology = $('#map_health_terminology_id');
+      terminology.find("option").remove();
+      $.each(result, function(item, element) {
+        terminology.append($("<option />").val(element.id).text(element.name));
+      });
+    });
+  });
 });
