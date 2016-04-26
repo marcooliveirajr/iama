678537ca1471fb41fea084bdec9e8a6ad5f8903d
andre <drelucas@gmail.com>
2016-04-25 21:39:55 -0300

    fix

--- app/assets/javascripts/health_terminology.js
+++ app/assets/javascripts/health_terminology.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
   $('#map_health_terminology_id').change(function(){
-    var insurance_id = $('#map_health_insurance_id option:selected').val();
-    $.getJSON("/health_terminologies/version.json?version_id="+ insurance_id, function(result){
+    //var insurance_id = $('#map_health_insurance_id option:selected').val();
+    $.getJSON("/health_terminologies/version.json?version_id=1", function(result){
       var terminologies = $('#map_health_terminology_id');
       terminologies.find("option").remove();
       $.each(result, function(item, element) {
