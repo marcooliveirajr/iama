8e477b26dd13f359e7ee66defeb03bd1bd2c8377
andre <drelucas@gmail.com>
2016-05-31 19:47:42 -0300

    add code_tuss and measure in maps

--- app/assets/javascripts/health_terminology.js
+++ app/assets/javascripts/health_terminology.js
@@ -6,4 +6,33 @@
           $(".measure").text(result[0].measure);
         });
         }
+        var terminologies2 = $('#map_tuss2');      
+        if($('#map_tuss2').length && $(".tuss").length && terminologies2.val() != ""){
+          $.getJSON("/health_terminologies/terminologies.json?id="+terminologies2.val(), function(result){
+          $(".tuss2").text(result[0].code_tuss);
+          $(".measure2").text(result[0].measure);
+        });
+        }
+        var terminologies3 = $('#map_tuss3');      
+        if($('#map_tuss3').length && $(".tuss").length && terminologies3.val() != ""){
+          $.getJSON("/health_terminologies/terminologies.json?id="+terminologies3.val(), function(result){
+          $(".tuss3").text(result[0].code_tuss);
+          $(".measure3").text(result[0].measure);
+        });
+        }
+        var terminologies4 = $('#map_tuss4');      
+        if($('#map_tuss4').length && $(".tuss").length && terminologies4.val() != ""){
+          $.getJSON("/health_terminologies/terminologies.json?id="+terminologies4.val(), function(result){
+          $(".tuss4").text(result[0].code_tuss);
+          $(".measure4").text(result[0].measure);
+        });
+        }
+        var terminologies5 = $('#map_tuss5');      
+        if($('#map_tuss5').length && $(".tuss").length && terminologies5.val() != ""){
+          $.getJSON("/health_terminologies/terminologies.json?id="+terminologies5.val(), function(result){
+          $(".tuss5").text(result[0].code_tuss);
+          $(".measure5").text(result[0].measure);
+        });
+        }
+
       });
