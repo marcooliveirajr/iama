a621f5f0d82bd0359ed4b8e0638d020eee8b8829
andre <drelucas@gmail.com>
2016-07-05 20:11:03 -0300

    add select2 in map

--- app/assets/javascripts/selects.js
+++ app/assets/javascripts/selects.js
@@ -4,6 +4,11 @@
   	$("#map_tuss3").select2();
   	$("#map_tuss4").select2();
   	$("#map_tuss5").select2();
+  	$("#map_anesthetist_id").select2();
+  	$("#map_surgeon_id").select2();
+  	$("#map_health_insurance_id").select2();
+  	$("#map_hospital_id").select2();
+  	$("#map_patient_id").select2();
   });
 
 
