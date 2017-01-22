4361d7cb3f932335913de555d1df50b69f1259fa
andre <drelucas@gmail.com>
2017-01-21 23:01:35 -0200

    movement

--- app/assets/javascripts/datetimepicker.js
+++ app/assets/javascripts/datetimepicker.js
@@ -26,12 +26,14 @@
     });
     $('#receipt_day').datetimepicker({
       locale: 'pt-br',
+      format: 'L',
       widgetPositioning: {
             horizontal: 'auto',
             vertical: 'bottom'
          }
     });
     $('#time_movement').datetimepicker({
-      locale: 'pt-br'
+      locale: 'pt-br',
+      format: 'L',
     });
   });
--- app/assets/javascripts/selects.js
+++ app/assets/javascripts/selects.js
@@ -1,23 +1,80 @@
   $(document).ready(function() { 
-  	$("#map_health_terminology_id").select2();
-  	$("#map_tuss2").select2(); 
-  	$("#map_tuss3").select2();
-  	$("#map_tuss4").select2();
-  	$("#map_tuss5").select2();
-  	$("#map_anesthetist_id").select2();
-  	$("#map_anesthetist1").select2();
-  	$("#map_anesthetist2").select2();
-  	$("#map_surgeon_id").select2();
-  	$("#map_surgeon1").select2();
-  	$("#map_surgeon2").select2();
-  	$("#map_health_insurance_id").select2();
-  	$("#map_hospital_id").select2();
-  	$("#map_patient_id").select2();
-    $("#map_health_plan_id").select2();
-    $("#input_category_id").select2();
-    $("#output_category_id").select2();
-    $("#cash_movement_input_id").select2();
-    $("#cash_movement_output_id").select2();
+  	$("#map_health_terminology_id").select2({
+      placeholder: "Selecione um Procedimento",
+      allowClear: true
+    });
+  	$("#map_tuss2").select2({
+      placeholder: "Selecione um segundo Procedimento",
+      allowClear: true
+    }); 
+  	$("#map_tuss3").select2({
+      placeholder: "Selecione um terceiro Procedimento",
+      allowClear: true
+    });
+  	$("#map_tuss4").select2({
+      placeholder: "Selecione um quarto Procedimento",
+      allowClear: true
+    });
+  	$("#map_tuss5").select2({
+      placeholder: "Selecione um quinto Procedimento",
+      allowClear: true
+    });
+  	$("#map_anesthetist_id").select2({
+      placeholder: "Selecione um Anestesista",
+      allowClear: true
+    });
+  	$("#map_anesthetist1").select2({
+      placeholder: "Selecione um segundo Anestesista",
+      allowClear: true
+    });
+  	$("#map_anesthetist2").select2({
+      placeholder: "Selecione um terceiro Anestesista",
+      allowClear: true
+    });
+  	$("#map_surgeon_id").select2({
+      placeholder: "Selecione um Cirurgião",
+      allowClear: true
+    });
+  	$("#map_surgeon1").select2({
+      placeholder: "Selecione um segundo Cirurgião",
+      allowClear: true
+    });
+  	$("#map_surgeon2").select2({
+      placeholder: "Selecione um terceiro Cirurgião",
+      allowClear: true
+    });
+  	$("#map_health_insurance_id").select2({
+      placeholder: "Selecione um Convênio Médico",
+      allowClear: true
+    });
+  	$("#map_hospital_id").select2({
+      placeholder: "Selecione um Hospital",
+      allowClear: true
+    });
+  	$("#map_patient_id").select2({
+      placeholder: "Selecione um Paciente",
+      allowClear: true
+    });
+    $("#map_health_plan_id").select2({
+      placeholder: "Selecione um Plano",
+      allowClear: true
+    });
+    $("#input_category_id").select2({
+      placeholder: "Selecione uma Categoria",
+      allowClear: true
+    });
+    $("#output_category_id").select2({
+      placeholder: "Selecione uma Categoria",
+      allowClear: true
+    });
+    $("#cash_movement_input_id").select2({
+      placeholder: "Selecione uma Entrada",
+      allowClear: true
+    });
+    $("#cash_movement_output_id").select2({
+      placeholder: "Selecione uma Saída",
+      allowClear: true
+    });
   });
 
 
