4b5b9f7e93a6af6d19050d262a69ca581addc203
andre <drelucas@gmail.com>
2017-02-05 23:10:54 -0200

    add validates in movement

--- app/models/cash_movement.rb
+++ app/models/cash_movement.rb
@@ -1,24 +1,19 @@
 class CashMovement < ActiveRecord::Base
   belongs_to :input
   belongs_to :output
-  validates :input_id, presence: { message: 'Necessário informar uma Entrada ou Saída' }, unless: ->(cash_movement){cash_movement.output_id.present?}
-  validates :output_id, presence: { message: 'Necessário informar uma Entrada ou Saída' }, unless: ->(cash_movement){cash_movement.input_id.present?}
-  
-  #validates :input_id, presence: { message: 'Ne' }, if: ->(cash_movement){ !output_id.blank? && !input_id.blank? }
-  #validates :output_id, presence: { message: 'Ne' }, if: ->(cash_movement){ !cash_movement.input_id.blank? && !cash_movement.output_id.blank? }
-  
-  #validates :input_id, :presence => { message: 'Ne' }, :if => "(!input_id.blank? && !output_id.blank?)"
-  #validates_presence_of :output_id, :presence => { message: 'Ne' }, :if => "!(input_id.present?) & !(output_id.present?)"
-
-
-  validates :time_movement, presence: { message: 'Necessário informar uma Data de Movimento' }
   validates :movement_value, presence: { message: 'Necessário informar um Valor do Movimento' }
-	
-	validates :input_id, presence: { message: 'Ne' }, if: ->(cash_movement){ input_output_not_blank? }
-	#validates :output_id, presence: { message: 'Ne' }, if: ->(cash_movement){ input_output_not_blank? }
+  validates_numericality_of :movement_value
+  validates :time_movement, presence: { message: 'Necessário informar uma Data de Movimento' }
+  
+  validates :input_id, presence: { message: 'Necessário informar uma Entrada ou Saída' }, unless: ->(cash_movement){ cash_movement.output_id.present? }
+  validates :output_id, presence: { message: 'Necessário informar uma Entrada ou Saída' }, unless: ->(cash_movement){ cash_movement.input_id.present? }
+  
+	validate :input_output_not_blank
 
 	private
-	def input_output_not_blank?
-	  !(input_id.blank?) && !(output_id.blank?)
+	def input_output_not_blank
+	  if !(input_id.blank?) && !(output_id.blank?)
+	  	errors.add(:input_id, "Não é possível selecionar Entrada e Saída no mesmo Movimento")
+	  end
 	end
 end
