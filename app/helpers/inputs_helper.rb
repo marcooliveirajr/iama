c13857d07309060050f6d665a9dd1b3e9a03ab25
andre <drelucas@gmail.com>
2017-02-12 16:09:12 -0200

    loading inputs and outputs

--- app/helpers/inputs_helper.rb
+++ app/helpers/inputs_helper.rb
@@ -1,7 +1,8 @@
 module InputsHelper
 
-  def inputs_filter 
-	Input.joins(:category).where("input_type = 'Entrada'").all
+  def inputs_filter
+  	Input.joins(:category).select("inputs.id, CONCAT(categories.name, ' - ', inputs.name) as cat").where("input_type = 'Entrada'")
+  	#Input.joins(:category).where("input_type = 'Entrada'").all
   end
     
 end
--- app/helpers/outputs_helper.rb
+++ app/helpers/outputs_helper.rb
@@ -1,7 +1,8 @@
 module OutputsHelper
 
   def outputs_filter 
-	Output.joins(:category).where("input_type = 'Saída'").all
+	Output.joins(:category).select("outputs.id, CONCAT(categories.name, ' - ', outputs.name) as cat").where("input_type = 'Saída'")
+	#Output.joins(:category).where("input_type = 'Saída'").all
   end
   
 end
