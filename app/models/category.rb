b48a2a5e67fcd1a084029e533445cda1d76d3573
andre <drelucas@gmail.com>
2017-02-14 15:52:32 -0200

    validates input type

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ActiveRecord::Base
   has_many :input, dependent: :restrict_with_error
   has_many :output, dependent: :restrict_with_error
   validates :name, presence: { message: :required_name }
+  validates :input_type, presence: { message: 'Necessário informar Entrada/Saída' }
 end
