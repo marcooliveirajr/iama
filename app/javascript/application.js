489370d60a034e23326ebb618dbb3a77be193816
marcomconsulting <marco.oliveira.mconsulting@gmail.com>
2026-05-21 23:11:07 -0300

    fix: adiciona jQuery CDN, corrige Bootstrap HTTPS e simplifica importmap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,11 @@
 import "@hotwired/turbo-rails"
-import "controllers"
-
-import jQuery from "jquery"
-window.jQuery = jQuery
-window.$ = jQuery
 
 import "select2"
 import moment from "moment"
 window.moment = moment
 
 console.log("IAMA Application loaded with Importmaps!")
+
 
 // Controle do Sidebar (Menu Lateral) compatível com Turbo e Locastyle 3
 $(document).on('click', '.ls-show-sidebar', function (e) {
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,9 +2,6 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin "jquery" # @4.0.0
 pin "moment" # @2.30.1
 pin "select2" # @4.1.0
+
