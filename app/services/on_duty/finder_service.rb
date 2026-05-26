6f87367ce61761b245b0f8a9e5e270fcbbc998a7
marcomconsulting <marco.oliveira.mconsulting@gmail.com>
2026-05-25 21:06:23 -0300

    fix: corrige query SQL de busca por anestesista e data no plantonista, e alinhamento do label

--- app/services/on_duty/finder_service.rb
+++ app/services/on_duty/finder_service.rb
@@ -4,27 +4,25 @@ class OnDuty
       def find(params)
         params = params.to_unsafe_h.symbolize_keys
         if params[:text].present? || params[:start_date].present? || params[:end_date].present? || params[:date].present?
-  				$param_type = params[:type]
-  				case $param_type
-  				when 'anesthetist'
-            OnDuty.joins(:anesthetist).where("name like ?", "%#{params[:text]}%")
+          $param_type = params[:type]
+          case $param_type
+          when 'anesthetist'
+            OnDuty.joins(:anesthetist).where("anesthetists.name LIKE ?", "%#{params[:text]}%")
           when 'on_duty_date'
             if params[:start_date].present? && params[:end_date].present?
-              start_date = Date.parse(params[:start_date]).beginning_of_day
-              end_date = Date.parse(params[:end_date]).end_of_day
-              OnDuty.where(on_duty_date: start_date..end_date).all
+              start_date = Date.parse(params[:start_date])
+              end_date   = Date.parse(params[:end_date])
+              OnDuty.where("DATE(on_duty_date) >= ? AND DATE(on_duty_date) <= ?", start_date, end_date).all
             elsif params[:start_date].present?
-              start_date = Date.parse(params[:start_date]).beginning_of_day
-              OnDuty.where("on_duty_date >= ?", start_date).all
+              start_date = Date.parse(params[:start_date])
+              OnDuty.where("DATE(on_duty_date) >= ?", start_date).all
             elsif params[:end_date].present?
-              end_date = Date.parse(params[:end_date]).end_of_day
-              OnDuty.where("on_duty_date <= ?", end_date).all
-            elsif params[:date].present?
-              OnDuty.where("on_duty_date like ?", "#{Date.parse(params[:date])}%").all
+              end_date = Date.parse(params[:end_date])
+              OnDuty.where("DATE(on_duty_date) <= ?", end_date).all
             else
               OnDuty.all
             end
-  				end
+          end
         else
           OnDuty.all
         end
